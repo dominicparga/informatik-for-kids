7c84bee629c3a8d25069ef35867a8d605bbe9f5a
Parga Cacheiro, Dominic <dominic.parga@icloud.com>
2016-12-15 15:04:49 +0100

    Init project

---
+++ labyrinth-ui/src/main/java/labyrinth/ui/Main.java
@@ -0,0 +1,11 @@
+package labyrinth.ui;
+
+/**
+ * @author Dominic Parga Cacheiro
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
