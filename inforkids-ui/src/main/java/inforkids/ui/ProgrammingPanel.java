b2a90372e701c7b6492f2071b0a1c3b8c8b872bd
Parga Cacheiro, Dominic <dominic.parga@icloud.com>
2017-03-11 16:36:14 +0100

    Add programming panel functionality: Add lines of code by user pressing a button

--- inforkids-ui/src/main/java/inforkids/ui/ProgrammingPanel.java
+++ inforkids-ui/src/main/java/inforkids/ui/ProgrammingPanel.java
@@ -1,6 +1,9 @@
 package inforkids.ui;
 
+import inforkids.ui.programming.LineOfCode;
 import inforkids.utils.io.ImageLoader;
+import microtrafficsim.utils.concurrency.delegation.StaticThreadDelegator;
+import microtrafficsim.utils.concurrency.delegation.ThreadDelegator;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
@@ -12,37 +15,87 @@ import java.awt.*;
 public class ProgrammingPanel extends JPanel {
 
     private JButton addJButton;
+    private JPanel  lines;
 
     public ProgrammingPanel() {
         super();
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setLayout(new GridBagLayout());
         setBackground(Color.WHITE);
         setBorder(new LineBorder(Color.BLACK, 6));
 
-        addJButton = new JButton("+");
-        addJButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(addJButton);
-        addJButton.addActionListener(e -> System.out.println("press"));
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.weightx = 1;
+        constraints.gridx = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weighty = 0;
+
+        /* add line panel */
+        lines = new JPanel(new GridBagLayout());
+//        constraints.insets = new Insets(12, 12, 12, 6);
+        add(lines, constraints);
+
+
+        /* add button */
+        addJButton = createAddButton();
+//        constraints.insets = new Insets(12, 12, 12, 6);
+        add(addJButton, constraints);
+
+
+        /* add empty panel */
+        JPanel gap = new JPanel();
+        gap.setBackground(Color.RED);
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weighty = 1;
+//        constraints.insets = new Insets(12, 12, 12, 6);
+        add(gap, constraints);
     }
 
     private JButton createAddButton() {
 
-        Image image = ImageLoader.loadImage(getClass(), "/undefined.png");
-        image = image.getScaledInstance(42, 42, Image.SCALE_SMOOTH);
-        ImageIcon icon = new ImageIcon(image);
+        JButton button;
+
+        boolean withIcon = false;
+
+        if (!withIcon) {
+            button = new JButton("+");
+        } else {
+            Image image = ImageLoader.loadImage(getClass(), "/undefined.png");
+            image = image.getScaledInstance(42, 42, Image.SCALE_SMOOTH);
+            ImageIcon icon = new ImageIcon(image);
 
 
-        JButton button = new JButton();
-        button.setBorderPainted(false);
-        button.setBorder(null);
-        //button.setFocusable(false);
-        button.setMargin(new Insets(0, 0, 0, 0));
-        button.setContentAreaFilled(false);
-        button.setIcon(icon);
-        button.setRolloverIcon(icon);
-        button.setPressedIcon(icon);
-        button.setDisabledIcon(icon);
+            button = new JButton();
+            button.setBorderPainted(false);
+            button.setBorder(null);
+            //button.setFocusable(false);
+            button.setMargin(new Insets(0, 0, 0, 0));
+            button.setContentAreaFilled(false);
+            button.setIcon(icon);
+            button.setRolloverIcon(icon);
+            button.setPressedIcon(icon);
+            button.setDisabledIcon(icon);
+        }
 
+        button.addActionListener(e -> createAndAddLine());
         return button;
+    }
+
+    private void createAndAddLine() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1;
+        constraints.weighty = 0;
+        constraints.gridx = 0;
+        constraints.gridy = GridBagConstraints.RELATIVE;
+        constraints.insets = new Insets(6, 6, 6, 6);
+
+//        lines.add(createAddButton(), constraints);
+        lines.add(new LineOfCode(" 6"), constraints);
+        lines.add(new LineOfCode("12"), constraints);
+        lines.add(new LineOfCode("24"), constraints);
+        lines.add(new LineOfCode("42"), constraints);
+        updateUI();
     }
 }
